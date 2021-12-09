32a2a045f375c54d2ceac24f159d8a0654e57f6c
Abraham Jerry Kakooza <abrahamkakooza@gmail.com>
2021-12-09 12:20:24 +0300

    Commenting out some useful tips

--- pages/tts.js
+++ pages/tts.js
@@ -8,15 +8,22 @@ import TextField from '@mui/material/TextField';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import React, { useState, useEffect, useRef } from 'react'
 
+// This is to handle the generate button and prevent the auto event change 
 const submitMoreData = async e => {
     e.preventDefault()
 }
+
+// This is the main function to work this component out
 function TtsCard({ user }) {
 
-  const [sentence, setSentence] = useState("Abantu be gwanga lyattu Uganda")
-  const [sentence2, setSentence2] = useState("http://[::1]:5002/api/tts?text=Abantu be gwanga lyattu Uganda")
+  // These states are used to handle the sentence being typed
+  const [sentence, setSentence] = useState("Wandika wanno")
+  const [sentence2, setSentence2] = useState("http://[::1]:5002/api/tts?text=Wandika wanno")
 
+  // An input useRef will help to manage the audio whenever a user types in a new sentence
   const inputRef = useRef()
+
+  // This function handles the sentence variable state change and also with an inputRef churns the audio output
   const setURL = (value) => {
     setSentence(value)
     const urlappend2 = "http://[::1]:5002/api/tts?text=" + value
@@ -24,13 +31,14 @@ function TtsCard({ user }) {
     setSentence2(urlappend2)
   }
 
+  // This is the initial Audio component value, will change after the generate button is clicked
   const urlappend = "http://[::1]:5002/api/tts?text="
 
+  // Basic appending of a sentence to the above value
   var newUrl = urlappend + sentence
 
+  // This will help auto play the audio when one clicks the generate button, function is below
   const likeAudio = new Audio(newUrl);
-
-
   const playSound = audioFile => {
     audioFile.play();
   }
@@ -41,16 +49,22 @@ function TtsCard({ user }) {
 
       <form
           onSubmit={submitMoreData}>
-
+      {
+        // One can type in the sentence they want here to be translated to an audio
+      }
       <Box sx={{ mx: "auto", width: 500 }}>
         <TextField fullWidth label="sentence" id="sentence" onChange={e => setURL(e.target.value)} value={sentence} />
       </Box>
       
-      
+      {
+        // Some spacing to align out the components
+      }
       <Box sx={{ paddingTop: 2 }}>
         
       </Box>
-
+      {
+        // Generate Button 
+      }
       <Box sx={{ mx: "auto", width: 200 }}>
       <ButtonGroup disableElevation variant="contained">
           <Button type="submit" value="Create" onClick={() => playSound(likeAudio)} >Generate</Button>
@@ -63,9 +77,15 @@ function TtsCard({ user }) {
       </Box>
 
       <Box sx={{ mx: "auto", width: 400 }}>
+      {
+        // The audio component with the inputRef
+      }
       <audio ref={inputRef} controls>
         <source src={sentence2} />
       </audio>
+      {
+        // This is a sentence to double check your input sentence
+      }
       <li>{sentence2}</li>
       
       </Box>
