03f992bcac193c2adc11fb8186a466da84ab0d69
Abraham Jerry Kakooza <abrahamkakooza@gmail.com>
2022-02-18 09:47:19 +0300

    Added new database changes and user generated sentences endpoint

--- pages/api/usersentence/index.js
+++ pages/api/usersentence/index.js
@@ -3,10 +3,11 @@ import prisma from '../../../lib/prisma'
 // POST /api/post
 // Updated fields in body: name, email, sentence, metric, comment, model
 export default async function handle(req, res) {
-  const { name, email, sentence, metric, comment, model, inference_time, rtf, wav_length_seconds, evaluation_time, sentence_num } = req.body
-  const result = await prisma.evaluation_db_table.create({
+  const { name, email, sentence, metric, comment, model, inference_time, rtf, wav_length_seconds, evaluation_time} = req.body
+  const result = await prisma.user_input_sentences_db_table.create({
     data: {
       model_version_id: model,
+      text:sentence,
       inference_time: inference_time,
       rtf:rtf,
       wav_length_seconds:wav_length_seconds,
@@ -15,26 +16,19 @@ export default async function handle(req, res) {
       comment: comment,
       b64_audio_string:null,
       acceptance_tag:true,
-      sentences_db_table: {
-        connect: { id: sentence_num },
-      },
       individuals_data_db_table:{
         connectOrCreate: {
           where:  { email: email },
           create: {
             email: email,
             name: name,
-            evaluated_sentences_no:1,
+            evaluated_sentences_no:0,
             evaluated_models_array:"['v2']",
             total_evaluated_models:1,
-            evaluation_status:true,
+            evaluation_status:false,
           },
         }
       },
-    },
-    include: {
-      individuals_data_db_table: true,
-      sentences_db_table:true,
     },
   })
   res.json(result)
